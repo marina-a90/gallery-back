637ecb3a00f9531f46802337f4cc521b172cb3e6
marina-a90 <marinaandjelkovic456@gmail.com>
2019-04-15 18:53:59 +0200

    galleries tabela dodata i gallery model

---
+++ database/migrations/2019_04_15_164317_create_galleries_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateGalleriesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('galleries', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('title');
+            $table->text('decription');
+            $table->unsignedInteger('user_id');
+            $table->foreign('user_id')
+                ->references('id')
+                ->on('users')
+                ->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('galleries', function (Blueprint $table) {
+            $table->dropForeign('galleries_user_id_foreign');
+            $table->dropColumn('user_id');
+        });
+    }
+}
