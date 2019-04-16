388170f34e8b5c7a989c499bbc374ef8c3963998
marina-a90 <marinaandjelkovic456@gmail.com>
2019-04-16 10:48:06 +0200

    jwt dodat

--- routes/api.php
+++ routes/api.php
@@ -17,4 +17,16 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::resource('galleries', 'API\GalleriesController');
+Route::resource('galleries', 'API\GalleriesController')
+    // ->middleware('auth:api') 
+    ;
+
+Route::group([
+    'middleware' => 'api',
+    'prefix' => 'auth'
+], function ($router) {
+    Route::post('login', 'AuthController@login');
+    Route::post('logout', 'AuthController@logout');
+    Route::post('refresh', 'AuthController@refresh');
+    Route::post('me', 'AuthController@me');
+});
